5e8b2d1a3647448d42253d57b55e8da6f9662050
xStone2022 <111463191+xStone2022@users.noreply.github.com>
2025-10-17 07:55:00 -0300

    Add files via upload

--- sequencia-ecf.py
+++ sequencia-ecf.py
@@ -1,8 +1,25 @@
-print("*** Mini Questionário ***\n")
-nome = input("Qual seu nome?\n")
-idade = input("Qual a sua idade?\n")
-genero = input("Você é homem ou mulher?\n")
-endereco = input("Qual o seu endereço?\n")
+import os
+import sys #importa biblioteca para limpeza do terminal
 
-print("Olá, ", nome, "\n")
-print("\n")
+def leitura_dados(): #função destinada a criação de variáveis e leitura de dados no console
+    print("*** Mini Questionário ***\n")
+    nome = input("Qual seu nome?\n")
+    idade = input("Qual a sua idade?\n")
+    genero = input("Você é homem ou mulher?\n")
+    endereco = input("Qual o seu endereço?\n")
+
+    return nome, idade, genero, endereco #retorno de variáveis para outras funções
+
+def impressao_dados(nome, idade, genero, endereco): #função destinada a impressão de dados no console
+    print("*** Mini Questionário ***\n")
+    print("Olá, ", nome, "\n")
+    print("Idade: ", idade, "\n")
+    print("Gênero: ", genero, "\n")
+    print("Endereço: ", endereco, "\n")
+
+def iniciar_app(): #função criada para iniciar a aplicação chamando outras funções existentes
+    leitura_dados() #chamando função de leitura de dados
+    os.system('cls') #chamando função para limpeza de console
+    impressao_dados() #chamando função de impressão de dados
+
+iniciar_app()
